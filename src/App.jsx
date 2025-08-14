525e3cce39b2bea300884aa6dd1af0bfa35196e5
srashtibhaoriya <srashtibhadoriya4@gmail.com>
2025-08-14 12:59:56 +0530

    routing updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import LoginPage from './Pages/LoginPage/LoginPage'
-import ProfileFeed from './Pages/FeedPages/Feedpage'
-import Discover from './Pages/DiscoverPeopl/Discover'
-const App = () => {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./Pages/Sidebar/Sidebar";
+import Messages from "./Pages/Messages/Messsages";
+// Import your actual page components (replace placeholders later)
+// import Feed from "./pages/Feed";
+// import Messages from "./pages/Messages";
+// import Connections from "./pages/Connections";
+// import Profile from "./pages/Profile";
+import Discover from "./Pages/Discover/Discover";
+import ProfileFeed from "./Pages/ProfileFeedPages/ProfileFeed";
+import PublicFeed from "./Pages/FeedPage/PublicFeed";
+function App() {
   return (
-    // <Discover />
-    <ProfileFeed />
-    // <LoginPage />
-    // <LandingPage />
-  )
+    <Router>
+      <div className="flex">
+        {/* Sidebar always visible */}
+        <Sidebar />
+
+        {/* Main content changes with route */}
+        <main className="bg-gradient-to-br from-[#1B3C53] via-[#456882] to-[#1B3C53] md:ml-60 flex-1 p-4 min-h-screen ">
+          <div className="max-w-5xl mx-auto mt-6">
+            <Routes>
+              <Route path="/public-feed" element={<PublicFeed />} />
+              <Route path="/messages" element={<Messages />} />
+              {/* <Route path="/connections" element={<Connections />} /> */}
+              <Route path="/discover" element={<Discover />} />
+              <Route path="/profile-feed" element={<ProfileFeed />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
