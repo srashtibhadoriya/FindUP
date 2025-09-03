b5cb54ded47a2d6f8b3357000bd92f02096fbfb2
srashtibhaoriya <srashtibhadoriya4@gmail.com>
2025-09-03 09:45:12 +0530

    routing fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./Pages/Sidebar/Sidebar";
-import Messages from "./Pages/Messages/Messsages";
-// Import your actual page components (replace placeholders later)
-// import Feed from "./pages/Feed";
-// import Messages from "./pages/Messages";
-// import Connections from "./pages/Connections";
-// import Profile from "./pages/Profile";
-import Discover from "./Pages/Discover/Discover";
-import ProfileFeed from "./Pages/ProfileFeedPages/ProfileFeed";
-import PublicFeed from "./Pages/FeedPage/PublicFeed";
+// import LoginPage from "../Pages/LoginPage/LoginPage";
+import LoginPage from "../src/Pages/LoginPage/Loginpage"
+import Sidebar from "../src/Pages/Sidebar/Sidebar";
+import PublicFeed from "../src/Pages/FeedPage/PublicFeed";
+import Messages from "../src/Pages/Messages/Messsages";
+import Discover from "../src/Pages/Discover/Discover";
+import ProfileFeed from "../src/Pages/ProfileFeedPages/ProfileFeed";
+
 function App() {
   return (
     <Router>
-      <div className="flex">
-        {/* Sidebar always visible */}
-        <Sidebar />
+      <Routes>
+        {/* Login Route */}
+        <Route path="/" element={<LoginPage />} />
 
-        {/* Main content changes with route */}
-        <main className="bg-gradient-to-br from-[#1B3C53] via-[#456882] to-[#1B3C53] md:ml-60 flex-1 p-4 min-h-screen ">
-          <div className="max-w-5xl mx-auto mt-6">
-            <Routes>
-              <Route path="/public-feed" element={<PublicFeed />} />
-              <Route path="/messages" element={<Messages />} />
-              {/* <Route path="/connections" element={<Connections />} /> */}
-              <Route path="/discover" element={<Discover />} />
-              <Route path="/profile-feed" element={<ProfileFeed />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
+        {/* Sidebar + Nested Routes */}
+        <Route path="/app" element={<Sidebar />}>
+          <Route path="public-feed" element={<PublicFeed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile-feed" element={<ProfileFeed />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
